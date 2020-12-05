bfc4163e651d44cffd68815c58027f8168f4e617
InvesTarget <system@investarget.com>
2020-12-05 14:17:05 +0800

    Work on project list

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -1,9 +1,421 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'dva'
+import { Link, withRouter } from 'dva/router';
+import { 
+  i18n, 
+  hasPerm, 
+  formatMoney, 
+  isShowCNY, 
+} from '../utils/util';
+
+import { Input, Icon, Table, Button, Pagination, Popconfirm, Modal } from 'antd'
+// import LeftRightLayout from '../components/LeftRightLayout'
+
+// import { ProjectListFilter } from '../components/Filter'
+// import { Search } from '../components/Search';
+
+// import AuditProjectModal from '../components/AuditProjectModal'
+import { PAGE_SIZE_OPTIONS } from '../constants';
+import { ApiError } from '../utils/request';
+
+const ProjectListFilter = { defaultValue: {
+  tags: [],
+  country: [],
+  industries: [],
+  netIncome_USD_F: 0,
+  netIncome_USD_T: 500000000,
+  grossProfit_F: -200000000,
+  grossProfit_T: 200000000,
+  projstatus: [],
+  service: [],
+  // indGroup: [],
+  // takeUser: [],
+  // makeUser: [],
+  usertype: null,
+  user: [],
+} };
 
 class ProjectList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // const { page } = props.location.query;
+
+    const setting = this.readSetting()
+    const filters = setting ? setting.filters : ProjectListFilter.defaultValue
+    const search = setting ? setting.search : null
+    const page = setting && setting.page ? setting.page : 1
+    const pageSize = setting && setting.pageSize ? setting.pageSize: props.userPageSize;
+
+    this.state = {
+      filters,
+      search,
+      page,
+      pageSize,
+      total: 0,
+      list: [],
+      loading: false,
+
+      visible: false,
+      id: null,
+      currentStatus: null,
+      status: null,
+      sendEmail: false,
+      confirmLoading: false,
+      sendWechat: false,
+      discloseFinance: false,
+    }
+  }
+
+  handleFilt = (filters) => {
+    this.setState({ filters, page: 1 }, this.getProject)
+  }
+
+  handleReset = (filters) => {
+    this.setState({ filters, page: 1, search: null }, this.getProject)
+  }
+
+  handleSearch = (search) => {
+    this.setState({ search, page: 1 }, this.getProject)
+  }
+
+  handlePageChange = (page) => {
+    // this.props.router.push(`/app/projects/list?page=${page}`);
+    this.setState({ page }, this.getProject)
+  }
+
+  handlePageSizeChange = (current, pageSize) => {
+    this.setState({ pageSize, page: 1 }, this.getProject)
+  }
+
+  // 特殊处理
+  handleFinancialFilter = (filters) => {
+    var data = { ...filters }
+    if (data['netIncome_USD_F'] == ProjectListFilter.defaultValue['netIncome_USD_F'] &&
+        data['netIncome_USD_T'] == ProjectListFilter.defaultValue['netIncome_USD_T'] )
+    {
+      delete data['netIncome_USD_F']
+      delete data['netIncome_USD_T']
+    }
+    if (data['grossProfit_F'] == ProjectListFilter.defaultValue['grossProfit_F'] &&
+        data['grossProfit_T'] == ProjectListFilter.defaultValue['grossProfit_T'] )
+    {
+      delete data['grossProfit_F']
+      delete data['grossProfit_T']
+    }
+    return data
+  }
+
+  getProject = () => {
+    const { filters, search, page, pageSize } = this.state
+    const params = { ...this.handleFinancialFilter(filters), search, skip_count: (page-1)*pageSize, max_size: pageSize, lang: 'cn' }
+    this.setState({ loading: true })
+    api.getProj(params).then(result => {
+      const { count: total, data: list } = result.data
+      this.setState({ total, list, loading: false })
+    }, error => {
+      this.setState({ loading: false })
+      this.props.dispatch({
+        type: 'app/findError',
+        payload: error
+      })
+    })
+    this.writeSetting()
+  }
+
+  handleDelete = (id) => {
+    this.setState({ loading: true })
+    api.deleteProj(id).then(result => {
+      this.getProject()
+    }, error => {
+      this.setState({ loading: false })
+      this.props.dispatch({
+        type: 'app/findError',
+        payload: error
+      })
+    })
+  }
+
+  // audit project modal
+
+  openAuditProjectModal = (id, status) => {
+    this.setState({ visible: true, id, currentStatus: status, status, sendEmail: false, sendWechat: false, discloseFinance: false })
+  }
+
+  handleStatusChange = (status) => {
+    this.setState({ status })
+  }
+
+  handleDiscloseFinanceChange = (discloseFinance) => {
+    this.setState({ discloseFinance });
+  }
+
+  handleSendEmailChange = (sendEmail) => {
+    this.setState({ sendEmail })
+  }
+
+  handleSendWechatChange = sendWechat => {
+    this.setState({ sendWechat });
+  }
+
+  handleConfirmAudit = () => {
+    const { id, status, sendEmail, sendWechat, discloseFinance } = this.state
+    this.setState({ confirmLoading: true })
+    api.editProj(id, { projstatus: status, isSendEmail: sendEmail, financeIsPublic: discloseFinance })
+      .then(result => {
+        this.setState({ visible: false, confirmLoading: false })
+        this.getProject()
+        if (sendWechat) {
+          api.sendProjPdfToWechatGroup(id);
+        }
+      })
+      .catch(error => {
+        this.setState({ visible: false, confirmLoading: false })
+        this.props.dispatch({
+          type: 'app/findError',
+          payload: error,
+        })
+      })
+  }
+
+  handleCancelAudit = () => {
+    this.setState({ visible: false })
+  }
+
+
+
+  writeSetting = () => {
+    const { filters, search, page, pageSize } = this.state
+    const data = { filters, search, page };
+    localStorage.setItem('ProjectList', JSON.stringify(data))
+  }
+
+  readSetting = () => {
+    var data = localStorage.getItem('ProjectList')
+    return data ? JSON.parse(data) : null
+  }
+
+  componentDidMount() {
+    this.getProject()
+  }
+
+  // componentWillReceiveProps(nextProps) {
+  //   const { page: nextPage } = nextProps.location.query;
+  //   const { page: currentPage } = this.props.location.query;
+  //   if (nextPage !== currentPage) {
+  //     this.setState({ page: parseInt(nextPage, 10) || 1 }, this.getProject);
+  //   }
+  // }
+
   render() {
-    return <h1>平台项目</h1>
+    const { location } = this.props
+    const { total, list, loading, page, pageSize, filters, search, visible, currentStatus, status, sendEmail, confirmLoading, sendWechat, discloseFinance } = this.state
+    const buttonStyle={textDecoration:'underline',border:'none',background:'none',width:'110px',textAlign:'left'}
+    const imgStyle={width:'15px',height:'20px'}
+    const columns = [
+      {
+        title: i18n('project.image'),
+        key: 'image',
+        render: (text, record) => {
+          const industry = record.industries && record.industries[0]
+          const imgUrl = industry ? industry.url : 'defaultUrl'
+          return (
+            <img src={imgUrl} style={{width: '80px', height: '50px'}} />
+          )
+        }
+      },
+      {
+        title: i18n('project.name'),
+        key: 'title',
+        render: (text, record) => {
+          if (record.action.get) {
+            return (
+              <span className="span-title">
+                <Link to={'/app/projects/' + record.id}>{record.projtitle}</Link>
+              </span>
+            )
+          } else {
+            this.props.dispatch({
+              type: 'app/findError',
+              payload: new ApiError(3000),
+            });
+          }
+        }
+      },
+      {
+        title: i18n('project.country'),
+        key: 'country',
+        render: (text, record) => {
+          const country = record.country
+          const countryName = country ? country.country : ''
+          let imgUrl = country && country.key && country.url
+          if (country && !imgUrl) {
+            const parentCountry = this.props.country.filter(f => f.id === country.parent)[0]
+            if (parentCountry && parentCountry.url) {
+              imgUrl = parentCountry.url
+            }
+          }
+          return (
+            <span style={{display: 'flex', alignItems: 'center', whiteSpace: 'nowrap'}}>
+              { imgUrl ? <img src={imgUrl} style={{width: '20px', height: '14px', marginRight: '4px'}} /> : null }
+              <span>{countryName}</span>
+            </span>
+          )
+        }
+      },
+      {
+        title: i18n('project.transaction_size'),
+        key: 'transactionAmount',
+        render: (text, record) => {
+          if (isShowCNY(record, this.props.country)) {
+            return record.financeAmount ? formatMoney(record.financeAmount, 'CNY') : 'N/A'
+          } else {
+            return record.financeAmount_USD ? formatMoney(record.financeAmount_USD) : 'N/A'
+          }
+        }       
+      },
+      {
+        title: i18n('project.current_status'),
+        key: 'projstatus',
+        render: (text, record) => {
+          const status = record.projstatus
+          const statusName = status ? status.name : ''
+          return statusName
+        }
+      }
+    ]
+    if (hasPerm('usersys.as_admin')) {
+      columns.push({
+        title: i18n('project.is_hidden'),
+        key: 'isHidden',
+        render: (text, record) => {
+          return record.isHidden ? i18n('project.invisible') : i18n('project.visible')
+        }
+      })
+    }
+    columns.push({
+        title: i18n('common.operation'),
+        key: 'action',
+        render: (text, record) => {
+          return (
+            <span  style={{display:'flex',alignItems:'center'}}>
+            <div style={{display:'flex',flexWrap:"wrap",maxWidth:'250px'}}>
+              <Button style={buttonStyle} disabled={!hasPerm('proj.admin_changeproj')} onClick={this.openAuditProjectModal.bind(this, record.id, record.projstatus.id)}>{i18n('project.modify_status')}</Button>
+
+              <Link href={"/app/projects/recommend/" + record.id} target="_blank">
+                <Button style={buttonStyle} disabled={!(record.projstatus.id >= 4 && record.projstatus.id < 8) || !(hasPerm('proj.admin_addfavorite') || hasPerm('usersys.as_trader'))}>{i18n('project.recommend')}</Button>
+              </Link>
+
+              {/* <Link to={"/app/timeline/add?projId=" + record.id}>
+                <Button style={buttonStyle}  disabled={!(record.projstatus.id >= 4 && record.projstatus.id < 8) || !(hasPerm('timeline.admin_addline') || hasPerm('timeline.user_addline'))}>{i18n('project.create_timeline')}</Button>
+              </Link> */}
+
+              { record.action.canAddOrgBD ? 
+              <Link to={"/app/orgbd/add?projId=" + record.id}>
+                <Button style={buttonStyle}  disabled={!(record.projstatus.id >= 4 && record.projstatus.id < 8)}>{i18n('project.create_org_bd')}</Button>
+              </Link>
+              : null }
+
+              { record.action.canAddMeetBD ? 
+              <Link to={"/app/meetingbd/add?projId=" + record.id}>
+                <Button style={buttonStyle}  disabled={!(record.projstatus.id >= 4 && record.projstatus.id < 8)}>{i18n('project.create_meeting_bd')}</Button>
+              </Link>
+              : null }
+
+              <Link to={'/app/dataroom/add?projectID=' + record.id}>
+                <Button style={buttonStyle} disabled={!record.action.canAddDataroom}>{i18n('project.create_dataroom')}</Button>
+              </Link>
+
+              { record.projstatus.id >= 4 && record.projstatus.id < 8 && (hasPerm('BD.manageOrgBD') || hasPerm('BD.user_getOrgBD')) ?
+              <Link to={'/app/org/bd?projId=' + record.id}>
+                <Button style={buttonStyle}>查看机构BD</Button>
+              </Link>
+              : null }
+
+              <Link to={'/app/projects/edit/' + record.id}>
+                <Button style={buttonStyle} disabled={!record.action.change}  >{i18n("common.edit")}</Button>
+              </Link>
+            </div>
+            <div>
+              <Popconfirm title={i18n('message.confirm_delete')} onConfirm={this.handleDelete.bind(null, record.id)}>
+                <Button size="small" style={buttonStyle} disabled={!record.action.delete}>
+                  <Icon type="delete" />
+                </Button>
+              </Popconfirm>
+            </div>
+            </span>
+          )
+        }
+    })
+    const action = (hasPerm('proj.admin_addproj') || hasPerm('proj.user_addproj')) ?
+                    { name: i18n('project.upload_project'), link: "/app/projects/add" } : null
+
+    return (
+      // <LeftRightLayout location={location} title={i18n('project.platform_projects')} action={action}>
+<div>
+        {/* <ProjectListFilter defaultValue={filters} onSearch={this.handleFilt} onReset={this.handleReset} /> */}
+
+        {/* <div style={{ marginBottom: 20, textAlign: 'right' }} className="clearfix">
+          <Search
+            style={{ width: 200 }}
+            placeholder={i18n('project.project_name')}
+            onSearch={this.handleSearch}
+            onChange={search => this.setState({ search })}
+            value={search}
+          />
+        </div> */}
+
+        <Table
+          columns={columns}
+          dataSource={list}
+          rowKey={record=>record.id}
+          loading={loading}
+          pagination={false}
+        />
+
+        <div style={{ margin: '16px 0' }} className="clearfix">
+          <Pagination
+            size="large"
+            style={{ float: 'right' }}
+            total={total}
+            current={page}
+            pageSize={pageSize}
+            onChange={this.handlePageChange}
+            showSizeChanger
+            onShowSizeChange={this.handlePageSizeChange}
+            showQuickJumper
+            pageSizeOptions={PAGE_SIZE_OPTIONS}
+          />
+        </div>
+
+        {/* <AuditProjectModal
+          projId={this.state.id}
+          visible={visible}
+          currentStatus={currentStatus}
+          status={status}
+          sendEmail={sendEmail}
+          confirmLoading={confirmLoading}
+          onStatusChange={this.handleStatusChange}
+          onSendEmailChange={this.handleSendEmailChange}
+          onOk={this.handleConfirmAudit}
+          onCancel={this.handleCancelAudit}
+          sendWechat={sendWechat}
+          onSendWechatChange={this.handleSendWechatChange}
+          discloseFinance={discloseFinance}
+          onDiscloseFinanceChange={this.handleDiscloseFinanceChange}
+        /> */}
+
+      {/* // </LeftRightLayout> */}
+      </div>
+    )
   }
 }
 
-export default ProjectList;
+function mapStateToProps(state) {
+  // const { country } = state.app
+  // const { page: userPageSize } = state.currentUser;
+  return { country: [], userPageSize: 10 };
+}
+
+
+export default connect(mapStateToProps)(withRouter(ProjectList));
---
+++ src/utils/util.js
@@ -0,0 +1,490 @@
+import { Popconfirm, Button, Modal } from 'antd'
+import * as api from '../api'
+window.api = api
+
+import { baseUrl } from './request'
+// import i18next from 'i18next'
+
+// Since IE doesn't support this we need the polyfill
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        // c. Increase k by 1.
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
+window.echo = function () {
+  const args = [...arguments];
+  args.unshift('%cLOG', 'color: white; font-weight: bold; background-color: red; padding: 3px')
+  console.log.apply(console, args);
+  // TODO: add caller position including file name and line number
+}
+
+// 使用 i18next 库来做国际化
+// let lang = window.appLocale.lang || 'cn'
+// i18next.init({
+//   lng: lang,
+//   debug: true,
+//   resources: {
+//     en: {
+//       translation: window.appLocale.messagesEn
+//     },
+//     cn: {
+//       translation: window.appLocale.messages
+//     }
+//   }
+// })
+
+window.LANG = localStorage.getItem('APP_PREFERRED_LANG') || 'cn';
+// changeLang(window.LANG);
+
+// export function changeLang(lang='en') {
+//   return new Promise((resolve, reject) => {
+//     i18next.changeLanguage(lang, (err, t) => {
+//       if (err) reject(err);
+//       else {
+//         window.LANG = lang;
+//         localStorage.setItem('APP_PREFERRED_LANG', lang);
+//         resolve(t);
+//       }
+//     });
+//   });
+// }
+
+// function i18n(key, options=null) {
+//   let ans;
+//   if (options) {
+//     ans = i18next.t(key, options)
+//   } else {
+//     ans = i18next.t(key)
+//   }
+//   if (ans === key) return ans.substr(ans.lastIndexOf(".") + 1).split("_").map(w => w ? w[0].toLocaleUpperCase() + w.substr(1) : w).join(" ");
+//   else return ans;
+// }
+
+function i18n(key) {
+  return key;
+}
+
+function time(dateFromServer) {
+  if (!window.Intl || typeof window.Intl !== "object") {
+    return dateFromServer.slice(0, 16).replace('T', ' ')
+  }
+  const date = new Date(dateFromServer)
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  }
+  const locale = window.LANG === 'en' ? 'en-US' : 'zh-CN'
+  return new Intl.DateTimeFormat(locale, options).format(date)
+}
+
+export function timeWithoutHour(dateFromServer) {
+  if (!window.Intl || typeof window.Intl !== "object") {
+    return dateFromServer.slice(0, 16).replace('T', ' ')
+  }
+  const date = new Date(dateFromServer)
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  }
+  const locale = window.LANG === 'en' ? 'en-US' : 'zh-CN'
+  return new Intl.DateTimeFormat(locale, options).format(date)
+}
+
+export function parseDate(dateFromServer) {
+  if (!window.Intl || typeof window.Intl !== "object") {
+    return dateFromServer.slice(0, 16).replace('T', ' ')
+  }
+  const date = new Date(dateFromServer)
+  const options = {
+    month: 'numeric',
+    day: 'numeric',
+  }
+  const locale = window.LANG === 'en' ? 'en-US' : 'zh-CN'
+  return new Intl.DateTimeFormat(locale, options).format(date)
+}
+
+export function parseTime(dateFromServer) {
+  if (!window.Intl || typeof window.Intl !== "object") {
+    return dateFromServer.slice(0, 16).replace('T', ' ')
+  }
+  const date = new Date(dateFromServer)
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  }
+  const locale = window.LANG === 'en' ? 'en-US' : 'zh-CN'
+  return new Intl.DateTimeFormat(locale, options).format(date)
+}
+
+export function timeSlapFromNow(dateString) {
+  return (new Date(dateString).getTime() - Date.now()) / 1000;
+}
+
+function timeForIM(timestamp) {
+  const timeInMs = Date.now()
+  const date = new Date(timestamp)
+  const nowDate = new Date(timeInMs)
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const nowYear = nowDate.getFullYear();
+  const nowMonth = nowDate.getMonth();
+  const nowDay = nowDate.getDate();
+
+  let options
+  if (year === nowYear && month === nowMonth && day === nowDay) {
+    if (!window.Intl || typeof window.Intl !== "object") {
+      return `${hour}:${minutes}`
+    }
+    options = {
+      hour: 'numeric',
+      minute: 'numeric',
+    }
+  } else {
+    if (!window.Intl || typeof window.Intl !== "object") {
+      return `${year.slice(2)}-${month}-${day}`
+    }
+    options = {
+      year: '2-digit',
+      month: 'numeric',
+      day: 'numeric',
+    }
+  }
+  const locale = window.LANG === 'en' ? 'en-US' : 'zh-CN'
+  return new Intl.DateTimeFormat(locale, options).format(date)
+}
+
+
+export function getCurrencyFromId(id) {
+  const map = {
+    1: 'CNY', // 人民币
+    2: 'USD', // 美元
+    3: 'EUR', // 欧元
+    5: 'GBP', // 英镑
+    6: 'JPY', // 日元
+    7: 'KRW', // 韩元
+    12: 'CNY', // 人民币及美元
+  }
+  return map[id]
+}
+
+function getCurrencySign(currency) {
+  const map = {
+    'CNY': '\uffe5',
+    'USD': '$',
+    'EUR': '\u20ac',
+    'GBP': '\uffe1',
+    'JPY': '\uffe5',
+    'KRW': '\u20a9',
+  }
+  return map[currency]
+}
+
+export function getCurrencyFormatter(id) {
+  const currency = getCurrencyFromId(id)
+  const sign = getCurrencySign(currency)
+  return function(value) {
+    if (isNaN(value)) {
+      return sign + ' '
+    } else{
+      return sign + ' ' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    }
+  }
+}
+
+export function getCurrencyParser(id) {
+  const currency = getCurrencyFromId(id)
+  const sign = getCurrencySign(currency)
+  const re = (sign == '$') ? new RegExp('\\$\\s?|(,*)', 'g') : new RegExp('\\' + sign + '\\s?|(,*)', 'g')
+  return function(value) {
+    return value.replace(re, '')
+  }
+}
+
+
+var exchangeCache = {}
+
+function exchange(source) {
+  if (exchangeCache[source] != null) {
+    let rate = exchangeCache[source]
+    return new Promise(function(resolve, reject) {
+      resolve(rate)
+    })
+  } else {
+    let param = {
+      'tcur': 'USD',
+      'scur': source,
+    }
+    return api.getExchangeRate(param).then((result) => {
+      var rate = result.data.rate
+      exchangeCache[source] = rate
+      return rate
+    })
+  }
+}
+
+function getUserInfo() {
+  const userInfoStr = localStorage.getItem('user_info')
+  try {
+    return JSON.parse(userInfoStr)
+  } catch(e) {
+    return null
+  }
+}
+
+function checkPerm(perm) {
+  const userInfo = getUserInfo()
+  if (!userInfo) {
+    return false
+  }
+  return userInfo.permissions.includes(perm)
+}
+
+function isLogin() {
+  return getUserInfo()
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function formatMoney(value, currency) {
+  let result = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  if (currency == 'CNY') {
+    return '¥' + result
+  } else {
+    return '$' + result
+  }
+}
+
+function hasPerm(name) {
+  const userInfo = getUserInfo()
+  if (!userInfo) return false
+  const perms = userInfo.permissions
+  return perms.includes(name)
+}
+
+function getGroup() {
+  const userInfo = getUserInfo()
+  if (!userInfo) return null
+  const group = userInfo.groups[0]
+  return group && group.id
+}
+
+function getCurrentUser() {
+  const userInfo = getUserInfo()
+  if (!userInfo) return null
+  return userInfo.id
+}
+
+function getToken() {
+  const userInfo = getUserInfo()
+  if (!userInfo) return null
+  return userInfo.token
+}
+
+function getPdfUrl(id) {
+  const token = getToken()
+  return `${baseUrl}/proj/pdf/${id}/?acw_tk=${token}&lang=${window.LANG}`;
+}
+
+
+function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||0,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+
+const intersection = (xs,ys) => xs.filter(x => ys.some(y => x === y))
+const subtracting = (xs, ys) => xs.filter(x => !ys.includes(x))
+
+
+export function handleError(error) {
+  window.app._store.dispatch({
+    type: 'app/findError',
+    payload: error,
+  })
+}
+
+export function getImageUrl(key) {
+  return 'https://image.investarget.com/' + key
+}
+
+export { i18n, exchange, checkPerm, isLogin, getRandomInt, formatMoney, getUserInfo, hasPerm, getGroup, getCurrentUser, formatBytes, intersection, subtracting, time, timeForIM, getPdfUrl }
+
+export function isParent(node, parentNode) {
+  while (node != undefined && node != null && node.tagName.toUpperCase() != 'BODY') {
+    if (node == parentNode) {
+      return true
+    }
+    node = node.parentNode
+  }
+  return false
+}
+
+export function appendToArray(array, items) {
+  array = array.slice()
+  if (Array.isArray(items)) {
+    let len = items.length;
+    for (let i=0; i<len; i++) {
+      let item = items[i]
+      if (!array.includes(item)) {
+        array.push(item)
+      }
+    }
+  } else {
+    if (!array.includes(items)) {
+      array.push(items)
+    }
+  }
+  return array
+}
+
+export function removeFromArray(array, items) {
+  array = array.slice()
+  if (Array.isArray(items)) {
+    let len = items.length;
+    for (let i=0; i<len; i++) {
+      let item = items[i]
+      let index = array.indexOf(item)
+      if (index > -1) {
+        array.splice(index, 1)
+      }
+    }
+  } else {
+    let index = array.indexOf(items)
+    if (index > -1) {
+      array.splice(index, 1)
+    }
+  }
+  return array
+}
+
+export function checkMobile(mobile) {
+  return /^\d{6,20}$/.test(mobile);
+}
+
+export function isShowCNY(record, allArea) {
+  const { country: recordArea } = record;
+  const parentArea = allArea.filter(f => f.id === recordArea.parent)[0];
+  const isRecordAreaChina = ['中国', 'China'].includes(recordArea.country);
+  const isRecordParentAreaChina = parentArea ? ['中国', 'China'].includes(parentArea.country) : false;
+  return (isRecordAreaChina || isRecordParentAreaChina) && record.currency.id === 1;
+}
+
+export function checkRealMobile(mobile) {
+  return /^(13[0-9]|14[579]|15[0-3,5-9]|17[0135678]|18[0-9])[\d*]{8}$/.test(mobile);
+}
+
+export function getUserGroupIdByName(allGroups, name) {
+  const group = allGroups.filter(f => f.name === name);
+  if (group.length > 0) {
+    return group[0].id;
+  } else {
+    throw new Error('Group Not Found!!!');
+  }
+}
+
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
