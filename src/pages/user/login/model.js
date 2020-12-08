e4b3bf23a5689f57284ceafa5e8e03db9003e81b
InvesTarget <system@investarget.com>
2020-12-08 09:50:17 +0800

    Redirect to project list no matter what

--- src/pages/user/login/model.js
+++ src/pages/user/login/model.js
@@ -27,22 +27,23 @@ const Model = {
         const params = getPageQuery();
         let { redirect } = params;
 
-        if (redirect) {
-          const redirectUrlParams = new URL(redirect);
+        // if (redirect) {
+        //   const redirectUrlParams = new URL(redirect);
 
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
+        //   if (redirectUrlParams.origin === urlParams.origin) {
+        //     redirect = redirect.substr(urlParams.origin.length);
 
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            window.location.href = redirect;
-            return;
-          }
-        }
+        //     if (redirect.match(/^\/.*#/)) {
+        //       redirect = redirect.substr(redirect.indexOf('#') + 1);
+        //     }
+        //   } else {
+        //     window.location.href = redirect;
+        //     return;
+        //   }
+        // }
 
-        history.replace(redirect || '/app/project');
+        // history.replace(redirect || '/app/project');
+        history.replace('/app/project');
       } catch (error) {
         yield put({
           type: 'changeLoginStatus',
