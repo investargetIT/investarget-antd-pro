5839610486f6e93d6094b31018425dd4126ec304
InvesTarget <system@investarget.com>
2020-12-08 09:11:49 +0800

    No production api

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,6 +5,10 @@ const prodUrl = 'https://api.investarget.com';
 let devUrl = "http://apitest.investarget.com";
 // devUrl = 'http://192.168.1.201:8000';
 // devUrl = prodUrl;
+
+// currently no production
+prodUrl = devUrl;
+
 const baseUrl = process.env.NODE_ENV === 'development' ? devUrl : prodUrl;
 const mobileUploadUrl = process.env.NODE_ENV === 'development' ? 'http://mtest.investarget.com' : 'https://m.investarget.com'; 
 export { baseUrl, mobileUploadUrl, prodUrl }
